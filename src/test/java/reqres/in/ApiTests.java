033bc295b461cc0453a82bbf0756456775c0857d
Sergey Zubakha <qasergeyzubakha@yandex.ru>
2023-07-26 23:38:05 +0300

    add tests

--- src/test/java/reqres/in/ApiTests.java
+++ src/test/java/reqres/in/ApiTests.java
@@ -1,16 +1,99 @@
 package reqres.in;
 
 import org.junit.jupiter.api.Test;
-import static io.restassured.RestAssured.get;
-import static io.restassured.RestAssured.given;
 
-public class StatusTests {
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class ApiTests {
     @Test
-    void checkTotal() {
+    void successfulLoginTest() {
+        String authData = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\" }"; // BAD PRACTICE
+
         given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .body(authData)
                 .when()
-                .get("")
+                .post("https://reqres.in/api/login")
                 .then()
-                .body()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
     }
-} 
+
+    @Test
+    void getListUsersTest() {
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .when()
+                .get("https://reqres.in/api/users?page=2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("total", is(12));
+    }
+
+    @Test
+    void getSingleUserTest() {
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .when()
+                .get("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data.id", is(2))
+                .body("data.email", is("janet.weaver@reqres.in"))
+                .body("data.first_name", is("Janet"))
+                .body("data.last_name", is("Weaver"));
+    }
+
+    @Test
+    void postCreateUserTest() {
+        String user = "{ \"name\": \"morpheus\",\n" +
+                " \"job\": \"leader\" }"; // BAD PRACTICE
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .body(user)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("morpheus"))
+                .body("job", is("leader"));
+    }
+
+    @Test
+    void unsuccessfulLoginTest() {
+        String authData = "{\"email\": \"peter@klaven\"}";
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .body(authData)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+}
