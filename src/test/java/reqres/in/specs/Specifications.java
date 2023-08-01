df81c1f3e8b29b694a65104de74ed4fb15028065
Sergey Zubakha <qasergeyzubakha@yandex.ru>
2023-08-01 06:59:12 +0300

    optimise imports

--- src/test/java/reqres/in/specs/Specifications.java
+++ src/test/java/reqres/in/specs/Specifications.java
@@ -4,13 +4,12 @@ import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
-import static io.restassured.RestAssured.with;
 import static in.reqres.helpers.CustomAllureListener.withCustomTemplates;
+import static io.restassured.RestAssured.with;
 import static io.restassured.filter.log.LogDetail.BODY;
 import static io.restassured.filter.log.LogDetail.STATUS;
 import static io.restassured.http.ContentType.JSON;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
-import static org.hamcrest.Matchers.notNullValue;
 
 public class Specifications {
     public static RequestSpecification loginRequestSpec = with()
